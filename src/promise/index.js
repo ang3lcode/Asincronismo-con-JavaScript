83e7ee86545e319e9ad2641688f71b766717081b
Luis Angel Perez Melendez <angelmelen88@gmail.com>
2022-03-28 17:44:17 -0600

    clase 7

--- src/promise/index.js
+++ src/promise/index.js
@@ -12,3 +12,22 @@ const somethingWillHappen = () => {
 somethingWillHappen()
 .then(response => console.log(response))
 .catch(err => console.error(err));
+
+
+const somethingWillHappen2 = () => {
+    return new Promise((resolve, reject) => {
+        if (true) {
+            setTimeout(() => {
+                resolve('true');
+            }, 2000)
+        }
+        else {
+            const error = new Error('whooop!');
+            reject(error);
+        }
+    });
+};
+
+somethingWillHappen2 ()
+.then(response => console.log(response))
+.catch(err => console.log(err));
